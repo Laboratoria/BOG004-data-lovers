1db1e23113edccea258cfee2edcf5615039f1dc2
Milates12 <accscortes09@gmail.com>
2022-02-03 16:42:29 -0500

    Agregando comentario

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-
+//Ejemplo de comentario
 console.log(example, data);
