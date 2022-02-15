f0524a63c5053981aea57736a83e2db6e408c660
Lizeth Rodriguez <lizeth_775@hotmail.com>
2022-02-15 10:36:59 -0500

    Panel de busqueda y filtros

--- src/main.js
+++ src/main.js
@@ -3,29 +3,28 @@ let peliculas = [];
 // debugger
 fetch('./data/ghibli/ghibli.json')
     .then(response => response.json())
-    .then(data => peliculas = data) 
+    .then(data => peliculas = data.films) 
     .then(() => iterarPelicula(peliculas))
     .catch(error => {
         throw(error);
     })
 
     function iterarPelicula (peliculas){
-        const {films} = peliculas;
         const lineaTiempoSection = document.getElementById('animaciones');
         
         let HTMLfinal = '';
 
-        films.forEach(film => { 
+        peliculas.forEach(pelicula => { 
             HTMLfinal += `
             <div class="flip-card">
                     <div class="flip-card-inner">
                         <div class="flip-card-front" id="poster">
-                            <img src="${film.poster}" />
+                            <img src="${pelicula.poster}" />
                         </div>
                         <div class="flip-card-back">
-                            <h1 id="title">${film.title}</h1> 
-                            <p id="release_date">${film.release_date}</p> 
-                            <p class="description" id="description">${film.description}</p>
+                            <h1 id="title">${pelicula.title}</h1> 
+                            <p id="release_date">${pelicula.release_date}</p> 
+                            <p class="description" id="description">${pelicula.description}</p>
                         </div>
                     </div>
                 </div>
@@ -36,11 +35,11 @@ fetch('./data/ghibli/ghibli.json')
 
 
     let galeria = document.getElementById('galery');
-    document.getElementById('animaciones').style.display = 'none';
+    document.getElementById('galeria-animaciones').style.display = 'none';
 
     function mostrarAnimaciones (){
-        document.getElementById('animaciones').style.display = 'block';
+        document.getElementById('galeria-animaciones').style.display = 'flex';
         document.getElementById('Home').style.display = 'none';   
     }
 
-    galeria.addEventListener('click', mostrarAnimaciones)
+    galeria.addEventListener('click', mostrarAnimaciones) 
