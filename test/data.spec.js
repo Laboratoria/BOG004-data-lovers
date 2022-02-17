07f32caf0818be378cd998ae480c3e15b800707a
Francy Corredor <fran_ymile@hotmail.com>
2022-02-17 15:24:53 -0500

    Se cargan test de las funciones del data.js

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,79 @@
-import { example, anotherExample } from '../src/data.js';
+import { filtrarPeliculas, organizarPeliculasAZ, peliculasxAño  } from '../src/data.js';
 
+let muestraPeliculas = [
+{"title": "Castle in the Sky",
+  "release_date": "1986",
+},
+{ "title": "My Neighbor Totoro",
+  "release_date": "1988",
+},
+{ "title": "Kiki's Delivery Service",
+  "release_date": "1989",
+},
+{ "title": "Grave of the Fireflies",
+  "release_date": "1988",
+},
+{ "title": "Only Yesterday",
+  "release_date": "1991",
+},
+{"title": "Castle in the Sky",
+  "release_date": "1986",
+},
+];
 
-describe('example', () => {
+let resultadosPeliculas = [
+  {"title": "Castle in the Sky",
+    "release_date": "1986",
+  },
+  { "title": "My Neighbor Totoro",
+    "release_date": "1988",
+  },
+  { "title": "Kiki's Delivery Service",
+    "release_date": "1989",
+  },
+  { "title": "Grave of the Fireflies",
+    "release_date": "1988",
+  },
+  { "title": "Only Yesterday",
+    "release_date": "1991",
+  },
+  {"title": "Castle in the Sky",
+  "release_date": "1986",
+  },
+];
+
+describe('filtrarPeliculas', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof filtrarPeliculas).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns `filtrarPeliculas`', () => {
+    expect(filtrarPeliculas(muestraPeliculas, 'totoro')).toEqual(resultadosPeliculas);
+  });
+  
+});
+
+describe('organizarPeliculasAZ', () => {
+  it('is a function', () => {
+    expect(typeof organizarPeliculasAZ).toBe('function');
+  });
+
+  it('returns `organizarPeliculasAZ`', () => {
+    const result = organizarPeliculasAZ(muestraPeliculas);
+    expect(result[0].title).toEqual('Castle in the Sky');
+  });
+});
+
+describe('peliculasxAño', () => {
+  it('is a function', () => {
+    expect(typeof peliculasxAño).toBe('function');
+  });
+
+  it('returns `peliculasxAño`', () => {
+    const resultadoxAño = peliculasxAño(muestraPeliculas);
+    expect(resultadoxAño[5].release_date).toEqual('1991');
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
