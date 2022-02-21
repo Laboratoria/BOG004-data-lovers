a7bf3ab7d443f135cdcf586830158bdda96b8e90
Daniela1421 <daniela.ducuara1817@gmail.com>
2022-02-21 12:44:15 -0500

    Mostrando en pantalla función sort

--- src/data.js
+++ src/data.js
@@ -1,48 +1,11 @@
-import data from './data/lol/lol.js';
-let dataLOL = Object.values(data.data);
-
-const filterByKey = (role) => {
+export const filterByKey = (dataLOL, role) => {
   const arrayFilter = dataLOL.filter(champion => champion.tags.includes(role));
-  /*console.log("prueba", arrayFilter);*/
-  return arrayFilter;
-  
+  return arrayFilter; 
 }
 
-export default filterByKey;
-/*export default filterByName;
-
-
-
-
-
-
-
-
-
-
-
-
-/*const showData = {
-
-  seeKiller: (dataLOL) => {
-  const filterKiller = dataLOL.filter(onlyKillers => onlyKillers.tags("Assassin"));
-  console.log("prueba", seeKiller);
-  return 'seeKiller';
-  
-},
-}
-export const anotherExample = () => {
-  return 'OMG';
-};*/
-
-
-
-
-/*const seeKiller = dataLOL.filter(allKiller)
-allKiller = () => {
-  if()
-}
-console.log ("prueba", seeKiller);*/
+export const sortChampions = (dataLOL) => dataLOL.sort((item1, item2) => { 
+   return (item1.name > item2.name) ? -1 : 1
+})
 
 
 
