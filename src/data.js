36f9b2969e01f26b0e6587d4480ce57d55bbafe4
unknown <michell16v@gmail.com>
2022-02-28 19:16:46 -0500

    solucionando conflictos

--- src/data.js
+++ src/data.js
@@ -1,37 +1,37 @@
 export const filterByKey = (dataLOL, role) => {
   const arrayFilter = dataLOL.filter(champion => champion.tags.includes(role));
-  return arrayFilter; 
+  return arrayFilter;
 }
 
-export const sortChampions = (dataLOL) => dataLOL.sort((item1, item2) => { 
-   return (item1.name > item2.name) ? -1 : 1
+export const sortChampions = (dataLOL) => dataLOL.sort((item1, item2) => {
+  return (item1.name > item2.name) ? -1 : 1
 })
 
 export const statistics1 = (dataLOL) => {
-  let arrayAttack= []; 
-dataLOL.forEach((player) => {
-  const championAttack= player.info.attack;
- arrayAttack.push(championAttack);
-});
-  let sumAttack= arrayAttack.reduce((a, b) => a + b , 0);
-  let averageAttack= sumAttack/dataLOL.length;
+  let arrayAttack = [];
+  dataLOL.forEach((player) => {
+    const championAttack = player.info.attack;
+    arrayAttack.push(championAttack);
+  });
+  let sumAttack = arrayAttack.reduce((a, b) => a + b, 0);
+  let averageAttack = sumAttack / dataLOL.length;
   return averageAttack;
-//console.log(averageAttack);
+  //console.log(averageAttack);
 
 }
 
-export const statistics2= (dataLOL) => {
-  let arrayDamage= []; 
-dataLOL.forEach((player) => {
-  const championDamage= player.stats.attackdamage;
-  arrayDamage.push(championDamage);
-});
+export const statistics2 = (dataLOL) => {
+  let arrayDamage = [];
+  dataLOL.forEach((player) => {
+    const championDamage = player.stats.attackdamage;
+    arrayDamage.push(championDamage);
+  });
 
-  let sumDamage= arrayDamage.reduce((item1, item2) => item1 + item2 , 0);
-  let averageDamage= sumDamage/dataLOL.length;
-  let averageRounded= Math.round((averageDamage)* 100) / 100;
+  let sumDamage = arrayDamage.reduce((item1, item2) => item1 + item2, 0);
+  let averageDamage = sumDamage / dataLOL.length;
+  let averageRounded = Math.round((averageDamage) * 100) / 100;
   return averageRounded;
-//console.log(averageRounded);
+  //console.log(averageRounded);
 }
 
 
