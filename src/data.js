f5ab849099c0182584f64f186f8892b4955df81b
Dannit23 <danalexa23@gmail.com>
2022-03-08 23:15:30 -0500

    data.js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,30 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+//funcion de ordenar la data
+export const datosOrdenados = (arregloGhibli) => {
+  return arregloGhibli.sort((elemento1, elemento2) => {
+   return (elemento1.title > elemento2.title) ? 1 : -1 
+ })
+  
+ }; 
+ 
+ //funcion de filtrar la data
+ export const filtrarDatos = (arrayanimaciones) => {
+   const arrayFiltrado = arrayanimaciones.filter(
+     (animaciones) => parseInt(animaciones.rt_score) >= 96
+    
+   );
+   console.log(arrayFiltrado)
+   return arrayFiltrado; 
+   
+ };
+ 
+ //Funcion de calculo
+  export const calcularPromedio = (dataAnimaciones) => {
+     let suma = 0;
+   for (let i = 0; i < dataAnimaciones.length; i++) {
+    const puntaje= parseInt(dataAnimaciones[i].rt_score);
+    suma += puntaje;
+   }
+   let promedio = suma / dataAnimaciones.length;
+   return promedio;
+   };
+ 
